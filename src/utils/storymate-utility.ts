73505d58c2010f34633df144373870412d238b80
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-04-30 17:57:09 +0200

    Update @drincs/pixi-vn-storymate to version 0.1.2 and adjust storymate call label in MainMenu

--- src/utils/storymate-utility.ts
+++ src/utils/storymate-utility.ts
@@ -1,7 +1,7 @@
 import { importStoryMate } from "@drincs/pixi-vn-storymate";
 
 export async function importAllInkLabels() {
-    const files = import.meta.glob<{ default: string }>("../storymate/*.{json}");
+    const files = import.meta.glob<{ default: string }>("../storymate/*.json");
     const fileEntries = await Promise.all(
         Object.entries(files).map(async ([path]) => {
             const fileModule = await import(/* @vite-ignore */ path + "?raw");
